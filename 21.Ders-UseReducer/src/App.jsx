40223d9f99f0ab6239efeacc790686f797a16539
zafergunayonalan <zafergunay7@gmail.com>
2024-01-04 16:16:07 +0300

    Reducer Kullanımı

--- 21.Ders-UseReducer/src/App.jsx
+++ 21.Ders-UseReducer/src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import { useReducer } from "react";
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialValue;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialValue);
   return (
     <>
-      <div>
-        <h1>Vite + React</h1>
+      <div className="App">
+        <div>Sayı:{count}</div>
+        <button onClick={() => dispatch("increment")}>Arttır</button>
+        <button onClick={() => dispatch("decrement")}>Azalt</button>
+        <button onClick={() => dispatch("reset")}>Sıfırla</button>
       </div>
     </>
   );
