de5444a6e802740b612ea8c38e164b351d6a1848
zafergunayonalan <zafergunay7@gmail.com>
2024-01-07 18:29:09 +0300

    React Router kurulum

---
+++ 30.Ders-React-Router6/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
