a931b72e0a83754c55cd14339b1a9c8ba2ef9377
zafergunayonalan <zafergunay7@gmail.com>
2024-01-07 03:35:13 +0300

    Custom Hook Form Örneği

---
+++ 29.Ders-CustomHook-FormOrnegi/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PersonalInfo from "./PersonalInfo";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <PersonalInfo />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 29.Ders-CustomHook-FormOrnegi/src/PersonalInfo.jsx
@@ -0,0 +1,34 @@
+import useInput from "./useInput";
+
+function PersonalInfo() {
+  //   const [firstName, setFirstName] = useState("");
+  //   const [lastName, setLastName] = useState("");
+
+  const [firstName, bindFirstName, resetFirstName] = useInput("");
+  const [lastName, bindLastName, resetLastName] = useInput("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    alert(`Merhaba ${firstName} ${lastName}`);
+    resetFirstName();
+    resetLastName();
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <div>
+          <label>Ad</label>
+          <input type="text" {...bindFirstName} />
+        </div>
+        <div>
+          <label>Soyad</label>
+          <input type="text" {...bindLastName} />
+        </div>
+        <button>Kaydet</button>
+      </form>
+    </div>
+  );
+}
+
+export default PersonalInfo;
