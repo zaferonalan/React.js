32f83b7458ad9e6c22a428b584df7c69fb5af28e
zafergunayonalan <zafergunay7@gmail.com>
2024-01-07 21:47:09 +0300

    useSearchParams ile filtreleme işlemi

--- 30.Ders-React-Router6/src/components/Members.jsx
+++ 30.Ders-React-Router6/src/components/Members.jsx
@@ -1,8 +1,19 @@
+import { useSearchParams } from "react-router-dom";
 function Members() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get("filter") === "active";
   return (
-    <div>
-      <h1>Members</h1>
-    </div>
+    <>
+      <div>
+        <h1>Members</h1>
+      </div>
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Aktif Üyeler
+      </button>
+      <button onClick={() => setSearchParams()}>Tüm Üyeler</button>
+
+      {isActive ? <h2>Aktif Üyeler</h2> : <h2>Tüm Üyeler</h2>}
+    </>
   );
 }
 
