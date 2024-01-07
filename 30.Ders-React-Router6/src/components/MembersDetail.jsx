c4f60be304103857348b44b246331ea49b486fe7
zafergunayonalan <zafergunay7@gmail.com>
2024-01-07 21:19:17 +0300

    Dinamik routerı useParamsla nasıl kullanılır.

--- 30.Ders-React-Router6/src/components/MembersDetail.jsx
+++ 30.Ders-React-Router6/src/components/MembersDetail.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+
 function MembersDetail() {
+  //   const params = useParams();
+  //   const memberId = params.memberId;
+  const { memberId } = useParams();
+
   return (
     <div>
-      <h1>MembersDetail</h1>
+      <h1>MembersDetail {memberId}</h1>
     </div>
   );
 }
