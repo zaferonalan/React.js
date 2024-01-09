476b36c1ab651835fab4bd2214ef6636ca35cefa
zafergunayonalan <zafergunay7@gmail.com>
2024-01-09 17:30:41 +0300

    Hakkımızda Ve İletişim Sayfası yapıldı

--- 31.Ders-BurgerProjesi/src/components/About.jsx
+++ 31.Ders-BurgerProjesi/src/components/About.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../style/About.css";
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos aperiam
+          unde eaque nihil at? Nam nihil veritatis reprehenderit magni fuga quo,
+          assumenda accusamus labore quae error, quidem, blanditiis dignissimos
+          similique. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nam suscipit in ab voluptatum officiis vero nisi beatae accusantium
+          dolorum, assumenda, officia, velit earum illum fugiat nemo rem aperiam
+          alias dolores. Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Error, et. Quis cum repudiandae officia nemo, odit rem natus
+          earum aliquam dolor sint hic illo mollitia nesciunt impedit quasi
+          cupiditate. Rem. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Quaerat, nostrum ad? Explicabo aliquam, tenetur voluptas est
+          amet eius excepturi doloribus blanditiis libero, provident, expedita
+          nam veniam laborum repellendus iusto vel?Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Ullam, culpa vel repellendus nesciunt
+          maiores perferendis repudiandae. Eius eligendi ratione, illum
+          perspiciatis ipsam culpa laborum molestias aut nemo quibusdam tempore.
+          Animi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
+          aut architecto facilis suscipit soluta dolorum, sint, sit quas odio
+          eveniet non quos, alias unde reiciendis vel doloribus! Minus, veniam
+          voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptas cupiditate dolore aliquid consequuntur quod nobis quas
+          reprehenderit corrupti, porro in ut hic, autem commodi assumenda quasi
+          molestias iusto eaque fuga! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Natus illo eligendi eaque maxime aut asperiores cum
+          perspiciatis expedita optio et quia unde at sunt consequatur,
+          blanditiis nam doloremque beatae sapiente!
+        </p>
+      </div>
     </div>
   );
 }
--- 31.Ders-BurgerProjesi/src/components/Contact.jsx
+++ 31.Ders-BurgerProjesi/src/components/Contact.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import "../style/Contact.css";
+import BannerImage from "../assets/banner.png";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen adınızı soyadınızı giriniz..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen email giriniz..."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            type="email"
+            name="email"
+            placeholder="Lütfen mesajınızı giriniz..."
+          />
+        </form>
+      </div>
     </div>
   );
 }
