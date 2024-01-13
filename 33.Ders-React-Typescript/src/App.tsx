c6302e64628ab93b29d269d1d50d4d4daa7b5eef
zafergunayonalan <zafergunay7@gmail.com>
2024-01-13 19:03:18 +0300

    Type veya kullanımı

--- 33.Ders-React-Typescript/src/App.tsx
+++ 33.Ders-React-Typescript/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/Home";
 import Instructor from "./components/Instructor";
 import InstructorList from "./components/InstructorList";
+import Request from "./components/Request";
 
 function App() {
   const instructor = {
@@ -36,6 +37,7 @@ function App() {
         <Home name="Zafer Günay Önalan" courseNumber={15} isBest={true} />
         <Instructor instructorNameLastname={instructor} />
         <InstructorList instructorNameLastnameList={instructorList} />
+        <Request status="loading" />
       </div>
     </>
   );
---
+++ 33.Ders-React-Typescript/src/components/Request.tsx
@@ -0,0 +1,19 @@
+type RequestProps = {
+  status: "loading" | "success" | "error";
+};
+
+function Request(props: RequestProps) {
+  let message = "";
+  if (props.status === "loading") {
+    message = "Yükleniyor";
+  }
+  if (props.status === "success") {
+    message = "Başarılı";
+  }
+  if (props.status === "error") {
+    message = "Hata";
+  }
+  return <div>{message}</div>;
+}
+
+export default Request;
