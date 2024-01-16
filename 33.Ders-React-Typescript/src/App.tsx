ae4a38e0ebcddf13fc79502a9c469b2c0b37101b
zafergunayonalan <zafergunay7@gmail.com>
2024-01-16 19:18:56 +0300

    UserState Type kullanımı

--- 33.Ders-React-Typescript/src/App.tsx
+++ 33.Ders-React-Typescript/src/App.tsx
@@ -1,45 +1,47 @@
 import "./App.css";
-import EventActions from "./components/EventActions";
-import Home from "./components/Home";
-import Instructor from "./components/Instructor";
-import InstructorList from "./components/InstructorList";
-import Request from "./components/Request";
+// import EventActions from "./components/EventActions";
+// import Home from "./components/Home";
+// import Instructor from "./components/Instructor";
+// import InstructorList from "./components/InstructorList";
+import LoginCheck from "./components/LoginCheck";
+// import Request from "./components/Request";
 
 function App() {
-  const instructor = {
-    firstName: "Zafer Günay",
-    lastName: "Önalan",
-  };
-  const instructorList = [
-    {
-      firstName: "Merve",
-      lastName: "Önalan",
-    },
-    {
-      firstName: "Zerrin",
-      lastName: "Önalan",
-    },
-    {
-      firstName: "Meryem",
-      lastName: "Önalan",
-    },
-    {
-      firstName: "Onay",
-      lastName: "Önalan",
-    },
-    {
-      firstName: "Zafer Günay",
-      lastName: "Önalan",
-    },
-  ];
+  // const instructor = {
+  //   firstName: "Zafer Günay",
+  //   lastName: "Önalan",
+  // };
+  // const instructorList = [
+  //   {
+  //     firstName: "Merve",
+  //     lastName: "Önalan",
+  //   },
+  //   {
+  //     firstName: "Zerrin",
+  //     lastName: "Önalan",
+  //   },
+  //   {
+  //     firstName: "Meryem",
+  //     lastName: "Önalan",
+  //   },
+  //   {
+  //     firstName: "Onay",
+  //     lastName: "Önalan",
+  //   },
+  //   {
+  //     firstName: "Zafer Günay",
+  //     lastName: "Önalan",
+  //   },
+  // ];
   return (
     <>
       <div className="App">
-        <Home name="Zafer Günay Önalan" courseNumber={15} isBest={true} />
+        {/* <Home name="Zafer Günay Önalan" courseNumber={15} isBest={true} />
         <Instructor instructorNameLastname={instructor} />
         <InstructorList instructorNameLastnameList={instructorList} />
         <Request status="loading" />
-        <EventActions />
+        <EventActions /> */}
+        <LoginCheck/>
       </div>
     </>
   );
---
+++ 33.Ders-React-Typescript/src/components/LoginCheck.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+type UserType = {
+    name:string,
+    email:string,
+    phone:string
+}
+
+function LoginCheck() {
+    const [user, setUser] = useState<UserType | null>({} as UserType)
+
+    const handleLogin = () => {
+        setUser({
+            name:"Zafer",
+            email:"ccc@gmail.com",
+            phone: "05555555"
+        })
+    }
+
+    const handleLogout = () => {
+        setUser({} as UserType)
+    }
+
+  return (
+    <div>
+        <button onClick={handleLogin} >Giriş Yap</button>
+        <button onClick={handleLogout} >Çıkış Yap</button>
+        
+        <div>
+            <p>{user?.name}</p>
+            <p>{user?.email}</p>
+            <p>{user?.phone}</p>
+        </div>
+    </div>
+  )
+}
+
+export default LoginCheck
