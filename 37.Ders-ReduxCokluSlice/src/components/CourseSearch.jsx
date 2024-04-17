8bf20a80775b5e2021f2ae67d7fc27e07a0bcaaf
zafergunayonalan <zafergunay7@gmail.com>
2024-04-17 14:17:48 +0300

    Components olusturuldu.

---
+++ 37.Ders-ReduxCokluSlice/src/components/CourseSearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseSearch = () => {
+  return (
+    <div>CourseSearch</div>
+  )
+}
+
+export default CourseSearch
---
+++ 37.Ders-ReduxCokluSlice/src/components/CourseValue.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseValue = () => {
+  return (
+    <div>CourseValue</div>
+  )
+}
+
+export default CourseValue
