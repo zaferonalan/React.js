40aa3c7e3dee95e35bfa64783e97d5fa3c5fc79b
zafergunayonalan <zafergunay7@gmail.com>
2024-04-18 11:49:13 +0300

    CourseList Listelendi

--- 37.Ders-ReduxCokluSlice/src/components/CourseList.jsx
+++ 37.Ders-ReduxCokluSlice/src/components/CourseList.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCourse } from '../store/slices/courseSlice';
 
 const CourseList = () => {
+  const dispatch = useDispatch()
+  const courses = useSelector((state) => {
+    return state.course.data
+  })
+
+  const renderedCourses = courses.map((course) => {
+    return(
+        <div key={course.id} className="panel">
+            <h1>{course.name}</h1>
+            <p>{course.description}</p>
+            <p>{course.cost}</p>
+            <button className='button is-danger' onClick={() => dispatch(removeCourse(course.id))} >Sil</button>
+        </div>
+    )
+  })
+
   return (
-    <div>CourseList</div>
+    <div className="courseList">
+        {renderedCourses}
+    </div>
   )
 }
 
--- 37.Ders-ReduxCokluSlice/src/store/slices/courseSlice.js
+++ 37.Ders-ReduxCokluSlice/src/store/slices/courseSlice.js
@@ -14,9 +14,16 @@ const courseSlice = createSlice({
                 cost: action.payload.cost,
                 id:nanoid()
             })
+        },
+        removeCourse(state,action){
+            const updatedCourses = state.data.filter((course) => {
+                return course.id !== action.payload
+            })
+            console.log(action.payload);
+            state.data = updatedCourses
         }
     }
 })
 
-export const { addCourse } = courseSlice.actions
+export const { addCourse, removeCourse } = courseSlice.actions
 export const courseReducer = courseSlice.reducer
