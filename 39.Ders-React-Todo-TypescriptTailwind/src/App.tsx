f44eb6ac389ae377991cc1940274c4040d3e00d2
zafergunayonalan <zafergunay7@gmail.com>
2024-04-27 16:04:34 +0300

    ListeOluşturuldu

--- 39.Ders-React-Todo-TypescriptTailwind/src/App.tsx
+++ 39.Ders-React-Todo-TypescriptTailwind/src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { FaPen, FaClipboardList } from "react-icons/fa";
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Zafer</h1>
+      <div className="header">
+        <div className="logoside">
+          <FaPen/>
+          <h1>What To Do</h1>
+          <FaClipboardList/>
+        </div>
+      </div>
+      <TodoList/>
     </div>
   )
 }
--- 39.Ders-React-Todo-TypescriptTailwind/src/components/TodoList.tsx
+++ 39.Ders-React-Todo-TypescriptTailwind/src/components/TodoList.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import TodoService from '../TodoServis'
 import TodoTypes from '../Todo'
+import { FaCheck, FaEdit } from "react-icons/fa";
+import { GiCancel} from "react-icons/gi";
+import { RiDeleteBin5Fill} from "react-icons/ri";
 
 const TodoList = () => {
   const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos())
@@ -35,14 +38,50 @@ const TodoList = () => {
 
 
   // ! DeleteTodo
-  const handleDelete = (id:number) => {
+  const handleDeleteTodo = (id:number) => {
     TodoService.deleteTodo(id);
     setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== id))
   }
 
   return (
     <div className="todoContainer">
-      
+      <div>
+        {/* { Todo Input From Component Goes Here } */}
+      </div>
+
+      {todos.map((todo) => (
+        <div className="items" key={todo.id}>
+          {editedTodoId == todo.id ? (
+            <div className="editedText">
+              
+              <input type="text" value={editedTodoText} 
+                onChange={(e) => setEditedTodoText(e.target.value)} 
+                autoFocus={true} 
+              />
+
+              <button onClick={() => handleEditSave(todo.id)}>
+                <FaCheck/>
+              </button>
+
+              <button className='cancelBtn' onClick={() => handleEditCancel()}>
+                <GiCancel/>
+              </button>
+
+            </div>
+          ): (
+            <div className="editBtn">
+              <span>{todo.text}</span>
+              <button onClick={() => handleEditStart(todo.id, todo.text)}>
+                <FaEdit/>
+              </button>
+            </div>
+          )}
+          <button onClick={() => handleDeleteTodo(todo.id)}>
+            <RiDeleteBin5Fill/>
+          </button>
+        </div>
+      ))}
+
     </div>
   )
 }
