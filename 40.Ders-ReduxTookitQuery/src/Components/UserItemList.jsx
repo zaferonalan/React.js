74fd166d16a7c352ea5097f5bf865852b0f904c3
zafergunayonalan <zafergunay7@gmail.com>
2024-05-21 16:38:18 +0300

    userRemove

--- 40.Ders-ReduxTookitQuery/src/Components/UserItemList.jsx
+++ 40.Ders-ReduxTookitQuery/src/Components/UserItemList.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import ExpandablePanel from './ExpandablePanel'
 import AlbumList from './AlbumList'
 import { GoTrash } from "react-icons/go";
+import { useRemoveUsersMutation } from "../store/index";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const UserItemList = ({user}) => {
+  const [ removeUsers, results ] = useRemoveUsersMutation();
+
+  const handleClick = () => {
+    debugger;
+    removeUsers(user)
+  }
+
   const header = (
     <>
-      <button style={{marginRight: '30px', border: 'none'}}>
-        <GoTrash/>
+      <button onClick={handleClick}  style={{marginRight: '30px', border: 'none', cursor: 'pointer'}}>
+        {results.isLoading ? (
+          <CircularProgress style={{width: '20px', height: '20px'}}/>
+        ): (<GoTrash/>)}
+        
       </button>
       {user.name}
     </>
