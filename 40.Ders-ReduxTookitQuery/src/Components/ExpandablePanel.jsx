7e6b98b7b547b89f6f6d14502c14d26d173c9b57
zafergunayonalan <zafergunay7@gmail.com>
2024-05-21 17:45:53 +0300

    album hazırlanması

--- 40.Ders-ReduxTookitQuery/src/Components/ExpandablePanel.jsx
+++ 40.Ders-ReduxTookitQuery/src/Components/ExpandablePanel.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 import { GoChevronLeft,GoChevronDown } from "react-icons/go";
 
 const ExpandablePanel = ({ children, header }) => {
+const [expandet, setExpandet] = useState(false)
+
+  const handleClick = () => {
+    setExpandet(!expandet)
+  }
+
   return (
     <div className='panelDiv'>
       <div className='topArrangement'>
         <div className='topArrangement'>{header}</div>
-        <div>
-          <GoChevronLeft/>
+        <div onClick={handleClick}>
+          {expandet ? <GoChevronDown/> : <GoChevronLeft/>}
+          
         </div>
       </div>  
-      
-      {children}
+
+      {/*! expandet true olduğu müddetçe. Soru işaretiyle de yapılabilir */}
+      { expandet && <div> {children} </div>}  
+
     </div>
   )
 }
