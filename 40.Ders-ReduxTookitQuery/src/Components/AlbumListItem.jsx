e23d7246e3298bf4d8be1fe0d207b510dd9cc33c
zafergunayonalan <zafergunay7@gmail.com>
2024-05-29 22:25:32 +0300

    albumListAndRemove

--- 40.Ders-ReduxTookitQuery/src/Components/AlbumListItem.jsx
+++ 40.Ders-ReduxTookitQuery/src/Components/AlbumListItem.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import ExpandablePanel from './ExpandablePanel'
+import FotoList from './FotoList'
+import { CircularProgress } from '@mui/material'
+import { GoTrash } from 'react-icons/go'
+import { useRemoveAlbumsMutation } from '../store'
 
 const AlbumListItem = ({ album }) => {
-    console.log(album)
+    const[removeAlbum, results] = useRemoveAlbumsMutation()
+
+    const handleClick = () => {
+        removeAlbum(album);
+    }
+
+    const header = (
+        <>
+            <button onClick={handleClick}  style={{marginRight: '30px', border: 'none', cursor: 'pointer'}}>
+            {results.isLoading ? (
+                <CircularProgress style={{width: '20px', height: '20px'}}/>
+            ): (<GoTrash/>)}
+            
+            </button>
+            {album.title}
+        </>
+        )
+
   return (
-    <div>{album.title}</div>
+    <div>
+        <ExpandablePanel header={header}>
+            <FotoList album={album}/>
+        </ExpandablePanel>
+    </div>
   )
 }
 
