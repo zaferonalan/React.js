9211e4f65413375c39eeb8653934646784eda815
zafergunayonalan <zafergunay7@gmail.com>
2024-06-01 01:06:45 +0300

    FotosApi

--- 40.Ders-ReduxTookitQuery/src/store/index.js
+++ 40.Ders-ReduxTookitQuery/src/store/index.js
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersApi } from "./api/usersApi";
 import { albumsApi } from "./api/albumsApi";
+import { photosApi } from "./api/fotosApi";
 
 export const store = configureStore({
     reducer: {
         [usersApi.reducerPath]: usersApi.reducer,
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer
     },
     middleware:(getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(usersApi.middleware).concat(albumsApi.middleware)
+        return getDefaultMiddleware().concat(usersApi.middleware).concat(albumsApi.middleware).concat(photosApi.middleware)
     },
 })
 
@@ -28,3 +30,9 @@ export {
     useAddAlbumsMutation,
     useRemoveAlbumsMutation
 }from './api/albumsApi'
+
+export {
+    useFetchPhotoQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation
+}from './api/fotosApi'
